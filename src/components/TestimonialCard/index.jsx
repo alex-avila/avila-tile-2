03bb8cbfc36162f7f64da656f46b2b51df519243
Alex Avila <alex.avilx@icloud.com>
2020-01-12 00:05:08 -0700

    add quotes to testimonial cards

--- src/components/TestimonialCard/index.jsx
+++ src/components/TestimonialCard/index.jsx
@@ -2,10 +2,17 @@ import React from "react"
 
 import styles from "./testimonial-card.module.sass"
 
+import quoteLeft from "../../images/quote-icon-left.svg"
+import quoteRight from "../../images/quote-icon-right.svg"
+
 const TestimonialCard = ({ testimonial, author }) => (
-  <div className={styles.card}>
-    <p>{testimonial}</p>
-    <div className={styles.author}>— {author}</div>
+  <div className={styles.cardWrapper}>
+    <img className={styles.quoteLeft} src={quoteLeft} alt />
+    <div className={styles.card}>
+      <p>{testimonial}</p>
+      <div className={styles.author}>— {author}</div>
+    </div>
+    <img className={styles.quoteRight} src={quoteRight} alt />
   </div>
 )
 
