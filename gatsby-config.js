5fd0e20f82fd4b186097c7b5ed1a5082f3b06f4c
Alex Avila <alex.avilx@icloud.com>
2020-01-11 17:38:26 -0700

    update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Avila Tile`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Family owned and operated, Avila Tile prides ourselves in delivering our best work to your business or residential improvement. Our clientele has built itself over the last 7 years based off of recommendation, professionalism and attention to detail.`,
   },
   plugins: [
     `gatsby-plugin-react-helmet-async`,
