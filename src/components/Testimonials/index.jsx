18fb0d6979aeadbc38436f0619688e7ab86ac069
Alex Avila <alex.avilx@icloud.com>
2020-01-11 23:30:56 -0700

    update testimonials section styles

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -1,19 +1,44 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
+import Img from "gatsby-image"
 import ContentWrapper from "@components/ContentWrapper"
 import FancyHeading from "@components/FancyHeading"
 import TestimonialsCarousel from "@components/TestimonialsCarousel"
 import styles from "./testimonials.module.sass"
 
-const Testimonials = () => (
-  <section className={styles.testimonials}>
-    <ContentWrapper>
-      <FancyHeading color="dark" alignment="center">
-        What our clients say
-      </FancyHeading>
-      <TestimonialsCarousel />
-    </ContentWrapper>
-  </section>
-)
+const Testimonials = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        imageSharp(
+          fluid: { originalName: { eq: "testimonials-background.jpg" } }
+        ) {
+          fluid(maxWidth: 1440) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <section className={styles.testimonials}>
+      <Img
+        className={styles.backgroundImage}
+        fluid={data.imageSharp.fluid}
+        imgStyle={{ objectPosition: "50% 60%" }}
+      />
+      <div className={styles.content}>
+        <ContentWrapper>
+          <FancyHeading color="dark" alignment="center">
+            What our clients say
+          </FancyHeading>
+          <TestimonialsCarousel />
+        </ContentWrapper>
+      </div>
+    </section>
+  )
+}
 
 export default Testimonials
