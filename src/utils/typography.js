fb216707e2073d5469fe876d88eea21a2782868b
Alex Avila <alex.avilx@icloud.com>
2020-03-29 22:15:52 -0600

    update scaleRatio to 2.77

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,7 +3,7 @@ import Typography from "typography"
 const typography = new Typography({
   baseFontSize: "16px",
   baseLineHeight: 1.5,
-  scaleRatio: 2,
+  scaleRatio: 2.77,
   headerFontFamily: ["Roboto", "sans-serif"],
   bodyFontFamily: ["Roboto", "sans-serif"],
   headerWeight: 300,
