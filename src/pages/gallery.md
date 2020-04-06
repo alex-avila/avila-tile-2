c68d1cb177ae55e083376698d215ea9f7c74b86d
Alex Avila <alex.avilx@icloud.com>
2020-04-05 20:07:26 -0600

    Make gallery page editable via Netlify CMS

--- src/pages/gallery.md
+++ src/pages/gallery.md
@@ -1,3 +1,30 @@
 ---
 templateKey: gallery-page
+heading: Gallery of our Work
+description: Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+types:
+  - All
+  - Kitchen
+  - Bathroom
+  - Floor
+  - Fireplace
+  - Specialty
+  - Commercial
+images:
+  - image: /images/testimonials-background.jpg
+    type: Kitchen
+  - image: /images/hero-background.jpg
+    type: Bathroom
+  - image: /images/IMG_20191231_155218_912.jpg
+    type: Commercial
+  - image: /images/IMG_20191221_165459_636.jpg
+    type: Bathroom
+  - image: /images/tammi 1.jpg
+    type: Bathroom
+  - image: /images/tammi 2.jpg
+    type: Bathroom
+  - image: /images/tammi 3.jpg
+    type: Bathroom
+  - image: /images/tammi 4.jpg
+    type: Bathroom
 ---
--- src/templates/gallery-page.jsx
+++ src/templates/gallery-page.jsx
@@ -17,11 +17,10 @@ const Gallery = ({
       <SEO title="Gallery" />
       <Projects
         theme="light"
-        heading="Gallery of Our Work"
-        copy="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit
-        odio, dapibus elit condimentum vitae. Maecenas eget orci vitae mollis."
-        images={frontmatter.projects.images}
-        types={frontmatter.projects.types}
+        heading={frontmatter.heading}
+        copy={frontmatter.description}
+        types={frontmatter.types}
+        images={frontmatter.images}
       />
       <FreeEstimateBanner />
     </Default>
@@ -32,37 +31,20 @@ export default Gallery
 
 export const pageQuery = graphql`
   query GalleryPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+    markdownRemark(frontmatter: { templateKey: { eq: "gallery-page" } }) {
       frontmatter {
-        hero {
-          heading
-          subheading
-        }
-        about {
-          heading
-          text
-        }
-        projects {
-          heading
-          text
-          images {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 350, sizes: "(max-width: 375px) 300px, 350px") {
-                  ...GatsbyImageSharpFluid_withWebp
-                }
+        heading
+        description
+        types
+        images {
+          image {
+            childImageSharp {
+              fluid(maxWidth: 350, sizes: "(max-width: 375px) 300px, 350px") {
+                ...GatsbyImageSharpFluid_withWebp
               }
             }
-            type
           }
-          types
-        }
-        testimonials {
-          heading
-          testimonials {
-            author
-            quote
-          }
+          type
         }
       }
     }
